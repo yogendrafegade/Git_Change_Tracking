62d202aec1d2b723a8eaede5b1f9d4bc0cf51a65
yogendrafegade <yog2800@gmail.com>
2026-03-23 18:19:07 +0530

    Query updated

--- query.sql
+++ query.sql
@@ -1,2 +1,2 @@
-select first_name as fname, last_name as lname, salary, department_id, job_id from employees e
-order by 1 asc, 3 desc;
+select first_name as fname, last_name as lname, salary, department_id, job_id from employees e where job_id in (select
+job_id from departments) order by 1 asc, 3 desc;
