5e690055e9d3532ce3a9c8e0d12c3d9e79607735
yogendrafegade <yog2800@gmail.com>
2026-03-23 13:37:46 +0530

    Query updated

--- query.sql
+++ query.sql
@@ -1,2 +1,2 @@
 select first_name, last_name, salary, department_id, job_id from employees where job_id =
-(select job_id from employee_id in (1,2,3,4,5));
+(select job_id from employees where employee_id in (1,2,3,4,5));
