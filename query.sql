2151274d5c4015d7fcc3075e41aba7f826ae5952
yogendrafegade <yog2800@gmail.com>
2026-03-23 16:16:45 +0530

    Update query for orderby clause

--- query.sql
+++ query.sql
@@ -1,1 +1,2 @@
-select first_name as fname, last_name as lname, salary, department_id, job_id from employees e;
+select first_name as fname, last_name as lname, salary, department_id, job_id from employees e
+order by 1 asc, 3 desc;
